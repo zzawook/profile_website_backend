c1b47cfb32c8b6e15f4b2c306036eeb098c5d77b
zzawook <kjaehyeok21@gmail.com>
2024-06-22 10:27:18 +0900

    Added initial spring project created by Spring initializr

---
+++ profile-website/src/test/java/dev/kjaehyeok21/profile_website/ProfileWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kjaehyeok21.profile_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
