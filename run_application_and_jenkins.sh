0510a552844273e11599ef3b0dcf62aa2e699856
zzawook <kjaehyeok21@gmail.com>
2024-07-24 14:56:10 +0800

    test

--- run_application_and_jenkins.sh
+++
@@ -1,7 +0,0 @@
-./scripts/clear_secrets.sh
-./scripts/fetch_mysql_secret.sh
-./scripts/fetch_redis_secret.sh
-# ./scripts/run_jenkins.sh
-
-docker compose up -d --build
-docker compose -f jenkins/compose.yaml up -d --build
