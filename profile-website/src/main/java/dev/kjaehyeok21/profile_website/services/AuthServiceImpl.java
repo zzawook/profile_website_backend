d2fddc59d4753389ce080241b31494be447334f8
zzawook <kjaehyeok21@gmail.com>
2024-08-03 00:40:55 +0800

    Modified the authService to use secret from AWS SecretsManager

--- profile-website/src/main/java/dev/kjaehyeok21/profile_website/services/AuthServiceImpl.java
+++ profile-website/src/main/java/dev/kjaehyeok21/profile_website/services/AuthServiceImpl.java
@@ -6,8 +6,21 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class AuthServiceImpl implements AuthService {
+
+    private String secret = "";
+
+    public AuthServiceImpl(AwsSecretsManagerService awsSecretsManagerService) {
+        try {
+            this.secret = awsSecretsManagerService.getSecret("auth_password");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error fetching Auth Password");
+            return;
+        }
+    }
+
     public Mono<Boolean> authByPassword(String password) {
-        return Mono.just(password.equals("password"));
+        return Mono.just(password.equals(this.secret));
     }
     
 }
